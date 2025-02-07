eedfe1ba2e326ac42db1748ae5255f7c25f45a85
tomfenno <treyfenno@gmail.com>
2025-02-07 14:03:57 -0700

    Code is working, I don't know if I have the equation correct though.

--- goal_prediction.py
+++ goal_prediction.py
@@ -1,21 +1,6 @@
 import numpy as np
-from copy import deepcopy
 from collections import deque
 import time
-
-# def predict_goal(grid, path):
-#     for i in range(len(path)):
-#         grid[path[i]] = 'x'
-#         for j in range(len(goals)):
-#             user_to_goals.append(bfs(grid, path[i], goals[j], deepcopy(dummy_path)))
-            
-#             probabilities.append((prob, i + 1))
-#         print(grid)
-#         print("Step", i, ":")
-#         print("Start to goals:", start_to_goals)
-#         print("User to goals:", user_to_goals)
-#         print("Probabilities:", probabilities[0])
-#         time.sleep(5)
 
 def bfs(matrix, start, end):
     path = []
@@ -26,8 +11,8 @@ def bfs(matrix, start, end):
             return steps
         directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
         for direction in directions:
-            if in_bounds(matrix, tuple(np.add((i, j), direction)), path):
-                path.append(tuple(np.add((i, j), direction)))
+            if in_bounds(matrix, (i + direction[0], j + direction[1]), path):
+                path.append((i + direction[0], j + direction[1]))
                 queue.append((i + direction[0], j + direction[1], steps + 1))
     return float('-inf')  
 
@@ -50,31 +35,31 @@ def player_move(move):
                 return (0, -1)
     
 def main():
-    goals = []
-    print("Please specify the dimensions of the grid:")
-    rows, columns = map(int, input().split())
-    print("Grid is", rows, "x", columns)
-    print("Enter the number of goal states:")
-    num_of_goals = int(input())
-    print("For each goal, enter its coordinates:")
-    for i in range(num_of_goals):
-        x, y = map(int, input().split())
-        goals.append((x, y))
-    print("There is a goal at:")
-    for g in goals:
-        print(g)
-    print("Enter the coordinates of the start position:")
-    x, y = map(int, input().split())
-    start = (x, y)
-    print("The user will begin at:", start)
+    # Code for reading input for different types of grids/goals
+    # goals = []
+    # print("Please specify the dimensions of the grid:")
+    # rows, columns = map(int, input().split())
+    # print("Grid is", rows, "x", columns)
+    # print("Enter the number of goal states:")
+    # num_of_goals = int(input())
+    # print("For each goal, enter its coordinates:")
+    # for i in range(num_of_goals):
+    #     x, y = map(int, input().split())
+    #     goals.append((x, y))
+    # print("There is a goal at:")
+    # for g in goals:
+    #     print(g)
+    # print("Enter the coordinates of the start position:")
+    # x, y = map(int, input().split())
+    # start = (x, y)
+    # print("The user will begin at:", start)
 
-    # start_to_goals = []
-    # rows, columns = 4, 5
-    # goals = [(0,0), (0, 4)]
-    # path = [(3, 2), (2, 2), (1, 2), (0, 2), (0, 1), (0, 0), (1, 0), (2, 0), (2, 1), (2, 2), (2, 3), (2, 4)]
-    # start = path[0]
-    start_to_goals, user_to_goals, user_to_each_goal = [], [], []
-    g1_likelihoods, g2_likelihoods, g1_probs, g2_probs = [], [], [], []
+    # fixed values
+    rows, columns = 4, 5
+    goals = [(0,0), (0, 4)]
+    start = (3, 2)
+
+    start_to_goals, user_to_goals = [], []
 
     grid = np.full((rows, columns), '*', dtype='U1')
     grid[start] = 'S'
@@ -85,44 +70,27 @@ def main():
     path = []
     path.append(start)
 
-    # print("Probability of goal 1:", abs(((0) - start_to_goals[0]) / start_to_goals[0]))
-    # print("Probability of goal 2:", abs(((0) - user_to_goals[1]) / start_to_goals[1]))
-
-    move = ""
+    move = (0, 0)
+    user = start
 
     while move != "x":
-        i = 0
-        move = input()
-        path.append(tuple(np.add(path[i], player_move(move))))
+        print(grid)
         for goal in goals:
-            user_to_goals.append(bfs(grid, path[-1], goal))
-        goal1_likelihood = abs((len(path) - user_to_goals[0]) / start_to_goals[0])
-        goal2_likelihood = abs((len(path) - user_to_goals[1]) / start_to_goals[1])
-        print("Probability of goal 1:", round(goal1_likelihood / (goal1_likelihood + goal2_likelihood)), 2)
-        print("Probability of goal 2:", round(goal2_likelihood / (goal1_likelihood + goal2_likelihood)), 2)
+            user_to_goals.append(bfs(grid, user, goal))
+        goal1_prediction = abs(((len(path) - 1) - user_to_goals[0]) / start_to_goals[0])
+        goal2_prediction = abs(((len(path) - 1)  - user_to_goals[1]) / start_to_goals[1])
+        goal1_probability = round(goal1_prediction / (goal1_prediction + goal2_prediction), 2)
+        goal2_probability = round(goal2_prediction / (goal1_prediction + goal2_prediction), 2)
+        print("Goal 1 Probability:", goal1_probability)
+        print("Goal 2 Probability:", goal2_probability)
+
         user_to_goals.clear()
-        i += 1
-
-    # print(grid)
-    # print(start_to_goals)
-    # print(user_to_goals)
-
-    # for i in range(len(path)):
-    #     # prob = abs(((i) - user_to_goals[i][0]) / start_to_goals[i])
-    #     # print("Step", i, "Goal 1:", user_to_goals[i][0])
-    #     # print("Step", i, "Goal 2:", user_to_goals[i][1])
-    #     g1_likelihoods.append(abs(((i) - user_to_goals[i][0]) / start_to_goals[0]))
-    #     g2_likelihoods.append(abs(((i) - user_to_goals[i][1]) / start_to_goals[1]))
-
-    # for i in range(len(path)):
-    #     g1_probs.append(round(g1_likelihoods[i]/(g1_likelihoods[i] + g2_likelihoods[i]), 2))
-    #     g2_probs.append(round(g2_likelihoods[i]/(g1_likelihoods[i] + g2_likelihoods[i]), 2))
-
-    # print("G1 probs:", g1_probs)
-    # print("G2 probs:", g2_probs)
-    # print(grid)
-    # print(start_to_goals)
-    # print(user_to_goals)
+        move = player_move(input())
+        user = (path[-1][0] + move[0], + path[-1][1] + move[1])
+        path.append(user)
+        grid[user] = 'X'
+        print("The user is now at:", user) 
+        # time.sleep(1)
 
 if __name__ == "__main__":
     main()
